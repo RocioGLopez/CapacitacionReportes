0806218c6ad4bdc2aa654a3ad93ceb1382632936
Rocio López <guisell.alvarado@gmail.com>
2025-09-20 16:21:09 -0600

    repositorio de capacitación de reportes usando cristal report https://www.youtube.com/watch?v=2n9yFQ3dVWM&t=7s&ab_channel=RiskoConsulting y lenguaje C#

---
+++ ReportesCapacitacion/ReportesCapacitacion/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ReportesCapacitacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Reporte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Reporte
+            // 
+            this.btn_Reporte.Location = new System.Drawing.Point(282, 208);
+            this.btn_Reporte.Name = "btn_Reporte";
+            this.btn_Reporte.Size = new System.Drawing.Size(230, 141);
+            this.btn_Reporte.TabIndex = 0;
+            this.btn_Reporte.Text = "Ver Reporte";
+            this.btn_Reporte.UseVisualStyleBackColor = true;
+            this.btn_Reporte.Click += new System.EventHandler(this.btn_Reporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Reporte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Reporte;
+    }
+}
+
